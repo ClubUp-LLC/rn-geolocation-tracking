f464524327264fabcc1595051470e3a1449b8d0b
rustamg <ryder.norco@gmail.com>
2024-08-23 20:51:53 +0400

    Fixed missing import error

--- android/src/main/java/us/clubup/geolocation/GeolocationService.java
+++ android/src/main/java/us/clubup/geolocation/GeolocationService.java
@@ -10,6 +10,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ServiceInfo;
 import android.content.res.TypedArray;
 import android.location.Location;
 import android.os.Build;
